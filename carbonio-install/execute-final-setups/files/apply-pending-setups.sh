3a4115374468a9f012af27d7c2e661b7c0c8abe5
Mary Tishchenkova <mary.tishchenkova@zextras.com>
2024-05-05 20:48:00 +0200

    chore: fix syntax

--- carbonio-install/execute-final-setups/files/apply-pending-setups.sh
+++ carbonio-install/execute-final-setups/files/apply-pending-setups.sh
@@ -5,12 +5,11 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 if [ $# -ne 1 ]; then
-	echo "${0} Service Discover Password"
-	exit 1
+        echo "${0} Service Discover Password"
+        exit 1
 fi
 
 export CONSUL_HTTP_TOKEN=$(echo $1| gpg --batch --yes --passphrase-fd 0 -qdo - /etc/zextras/service-discover/cluster-credentials.tar.gpg | tar xOf - consul-acl-secret.json | jq .SecretID -r);
 export SETUP_CONSUL_TOKEN=$(echo $1 | gpg --batch --yes --passphrase-fd 0 -qdo - /etc/zextras/service-discover/cluster-credentials.tar.gpg | tar xOf - consul-acl-secret.json | jq .SecretID -r);
 pending-setups -a
 su - zextras -c "/opt/zextras/bin/zmcontrol restart"
-
